1d3cebe0e4d434cdfae6390e872d8e69c571ab94
bissli <bissli@xyz.org>
2026-05-22 14:28:20 -0400

    Bump version: 0.1.5 -> 0.1.6

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -7,7 +7,7 @@ All query/data operations can be called either as:
 
 The module functions are facades for backwards compatibility.
 """
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 from typing import Any, TextIO
 
